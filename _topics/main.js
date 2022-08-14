f884130f337869b1ef7c848671287f84edc2124c
wcasper <williamrileycasper@gmail.com>
2022-08-14 14:45:19 -0700

    playing

--- _topics/main.js
+++ _topics/main.js
@@ -37,7 +37,7 @@ function makeColumns(cellNum) {
     for (i = 0; i < rows.length; i++) {
         for (j = 0; j < cellNum; j++) {
             let newCell = document.createElement("div");
-	    newcell.style.background = "red";
+	    newCell.style.background = "red";
             rows[j].appendChild(newCell).className = "cell";
         };
 
